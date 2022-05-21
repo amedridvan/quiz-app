a4bd8d11d761963a8505950a8043d596fd886364
amedridvan <ahmedridvan1@gmail.com>
2022-05-21 17:48:38 +0300

    code and photo

---
+++ src/Data/Categories.js
@@ -0,0 +1,31 @@
+const Categories = [
+  /*{
+    category: "General Knowledge",
+    value: 9,
+ // },*/
+ { category: "kitaplar", value: 10 },
+ // { category: "Films", value: 11 },
+ // { category: "Music", value: 12 },
+ // { category: "Musicals and Theaters", value: 13 },
+  { category: "Televizyon", value: 14 },
+  { category: "Video oyunu ", value: 15 },
+  { category: "Masa oyunu  ", value: 16 },
+  { category: "Bilim ve Doğa", value: 17 },
+  { category: "Bilgisayar ", value: 18 },
+  { category: "Matmatic", value: 19 },
+  //{ category: "Mythology", value: 20 },
+  { category: "Sporlar", value: 21 },
+ // { category: "Geography", value: 22 },
+ // { category: "History", value: 23 },
+  { category: "Siyaset", value: 24 },
+  // { category: "Art", value: 25 },
+ // { category: "Celebrities", value: 26 },
+ // { category: "Animals", value: 27 },
+ // { category: "Vehicles", value: 28 },
+//{ category: "Comics", value: 29 },
+  //{ category: "Gadgets", value: 30 },
+ // { category: "Japanese Anime", value: 31 },
+  //{ category: "Cartoon and Animations", value: 32 },
+];
+
+export default Categories;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="/"
+        style={{ cursor: "pointer" }}
+      >
+        ahmed rawdan 
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
